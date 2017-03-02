3b93d0054701939993eca96c4263f6655e7426f5
HOU <houtianle@gmail.com>
2017-03-02 13:22:15 +0100

    LIBFT

--- src/libc/ft_memccpy.c
+++ src/libc/ft_memccpy.c
@@ -12,8 +12,8 @@
 
 #include "libft.h"
 
-void	*ft_memccpy(void *restrict dst,
-		const void *restrict src, int c, size_t n)
+void	*ft_memccpy(void *dst,
+		const void *src, int c, size_t n)
 {
 	unsigned char	*a;
 	unsigned char	*b;
